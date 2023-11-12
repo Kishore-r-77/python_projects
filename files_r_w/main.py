5c6bb3be0c906b969e384ec858801c6e357c40de
Kishore-r-77 <kishorework77@gmail.com>
2023-11-12 20:28:27 +0530

    wrote a code for appending the text in a file

--- files_r_w/main.py
+++ files_r_w/main.py
@@ -1,3 +1,8 @@
-with open("file.txt") as text_file:
-    content = text_file.read()
-    print(content)
+# Reading the file
+# with open("file.txt") as text_file:
+#     content = text_file.read()
+#     print(content)
+
+# Appending the text
+with open("file.txt",mode="a") as text_file:
+    text_file.write("\nI Excel more than Anyone Around Me.")

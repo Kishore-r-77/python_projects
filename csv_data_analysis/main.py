7b16293b400b0166aae2ec79e3da9ee7e3978ceb
Kishore-r-77 <kishorework77@gmail.com>
2023-11-13 14:10:08 +0530

    converted temperature column to list using pandas

--- csv_data_analysis/main.py
+++ csv_data_analysis/main.py
@@ -12,6 +12,9 @@
 import pandas
 
 csv_data = pandas.read_csv("weather_data.csv")
-print(csv_data["temp"])
-csv_dict = csv_data.to_dict()
-print(csv_dict)
+
+# csv_dict = csv_data.to_dict()
+# print(csv_dict)
+
+temp_list=csv_data["temp"].to_list()
+print(temp_list)

7e1e088291b1deea6ca5cf771e9d04ddbc158f17
Kishore-r-77 <kishorework77@gmail.com>
2023-11-13 13:59:59 +0530

    added panda library to read the csv file

--- csv_data_analysis/main.py
+++ csv_data_analysis/main.py
@@ -1,11 +1,15 @@
 # with open("weather_data.csv") as csv_file:
 #     csv_data=csv_file.readlines()
 #     print(csv_data)
-import csv
-with open("weather_data.csv") as csv_file:
-    csv_data=csv.reader(csv_file)
-    temperature=[]
-    for row in csv_data:
-        if row[1]!="temp":
-            temperature.append(int(row[1]))
-    print(temperature)
+# import csv
+# with open("weather_data.csv") as csv_file:
+#     csv_data=csv.reader(csv_file)
+#     temperature=[]
+#     for row in csv_data:
+#         if row[1]!="temp":
+#             temperature.append(int(row[1]))
+#     print(temperature)
+import pandas
+
+csv_data=pandas.read_csv("weather_data.csv")
+print(csv_data)

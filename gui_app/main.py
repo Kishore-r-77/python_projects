a53559d024b3fe9f42e51ebf4587e8678e3e8cb5
Kishore-r-77 <kishorework77@gmail.com>
2023-11-15 20:04:46 +0530

    tested pack() place() and grid() layout methods

--- gui_app/main.py
+++ gui_app/main.py
@@ -5,25 +5,28 @@ window = tkinter.Tk()
 window.title("My First Gui App")
 window.minsize(width=500, height=300)
 
-# Label
-my_label = tkinter.Label(text="I'm a Label", font=("Ariel", 24, "bold"))
-my_label.pack()
-my_label["text"] = "New Text"
-
-my_label.config(text="Kishore")
-
 
 def button_clicked():
     print("I got Clicked")
     my_label.config(text=my_input.get())
 
 
+# Label
+my_label = tkinter.Label(text="I'm a Label", font=("Ariel", 24, "bold"))
+# my_label.pack()
+my_label["text"] = "New Text"
+my_label.config(text="Kishore")
+# my_label.place(x=0,y=0)
+my_label.grid(column=1, row=0)
+
 # Button
 my_button = tkinter.Button(text="Click me", command=button_clicked)
-my_button.pack()
+# my_button.pack()
+my_button.grid(column=1, row=2)
 
 # Entry
 my_input = tkinter.Entry(width=10)
-my_input.pack()
+# my_input.pack()
+my_input.grid(column=1, row=3)
 
 window.mainloop()

4bdfb721f9213a4b0d2c99636fbf1b60a3217cbb
Kishore-r-77 <kishorework77@gmail.com>
2023-11-15 10:45:07 +0530

    created button and click event

--- gui_app/main.py
+++ gui_app/main.py
@@ -8,5 +8,15 @@ window.minsize(width=500, height=300)
 # Label
 my_label = tkinter.Label(text="I'm a Label", font=("Ariel", 24, "bold"))
 my_label.pack()
+my_label["text"] = "New Text"
+my_label.config(text="Kishore")
 
+
+def button_clicked():
+    print("I got Clicked")
+
+
+# Button
+my_button = tkinter.Button(text="Click me", command=button_clicked)
+my_button.pack()
 window.mainloop()

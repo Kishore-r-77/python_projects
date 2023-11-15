0294f82c399401d7d12b4ed4abb50bbe9f016143
Kishore-r-77 <kishorework77@gmail.com>
2023-11-15 10:52:51 +0530

    Added Input Entry

--- gui_app/main.py
+++ gui_app/main.py
@@ -9,14 +9,21 @@ window.minsize(width=500, height=300)
 my_label = tkinter.Label(text="I'm a Label", font=("Ariel", 24, "bold"))
 my_label.pack()
 my_label["text"] = "New Text"
+
 my_label.config(text="Kishore")
 
 
 def button_clicked():
     print("I got Clicked")
+    my_label.config(text=my_input.get())
 
 
 # Button
 my_button = tkinter.Button(text="Click me", command=button_clicked)
 my_button.pack()
+
+# Entry
+my_input = tkinter.Entry(width=10)
+my_input.pack()
+
 window.mainloop()

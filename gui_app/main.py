23e40da37204e7f5989e27d008ae4700bb029ef6
Kishore-r-77 <kishorework77@gmail.com>
2023-11-15 08:36:49 +0530

    added font style

--- gui_app/main.py
+++ gui_app/main.py
@@ -6,7 +6,7 @@ window.title("My First Gui App")
 window.minsize(width=500, height=300)
 
 # Label
-my_label = tkinter.Label(text="I'm a Label")
+my_label = tkinter.Label(text="I'm a Label", font=("Ariel", 24, "bold"))
 my_label.pack()
 
 window.mainloop()

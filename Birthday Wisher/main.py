75a45d7cff65ffda4ae646de3412a6b107c53932
Kishore-r-77 <kishorework77@gmail.com>
2023-11-17 21:02:07 +0530

    made the code short so that we don't have to use connection.quit()

--- Birthday Wisher/main.py
+++ Birthday Wisher/main.py
@@ -16,16 +16,16 @@ to_address = "kishorelesnar77@gmail.com"
 password = "wzkpjrbpqbnkpiik"
 
 # Use SMTP_SSL and specify the port
-connection = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+with smtplib.SMTP_SSL("smtp.gmail.com", 465) as connection:
 
-# Login to the email account
-connection.login(user=my_email, password=password)
+    # Login to the email account
+    connection.login(user=my_email, password=password)
 
-# Send the email
-connection.sendmail(from_addr=my_email, to_addrs=to_address, msg="Subject: Test\n\nHello")
+    # Send the email
+    connection.sendmail(from_addr=my_email, to_addrs=to_address, msg="Subject: Test\n\nHello Supreme")
 
-# Close the connection
-connection.quit()
+# # Close the connection
+# connection.quit()
 
 
 

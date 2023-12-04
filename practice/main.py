ba6f44aeccc92a921222ef99931fb9d8bb4ad84d
Kishore-r-77 <kishorework77@gmail.com>
2023-12-04 20:44:16 +0530

    practiced order agnostic binary search in python

--- practice/main.py
+++ practice/main.py
@@ -1,20 +1,27 @@
-nums = [1, 2, 3, 4, 5]
+nums = [5, 4, 3, 2, 1]
 
 
-def swap(arr, pos1, pos2):
-    temp = arr[pos1]
-    arr[pos1] = arr[pos2]
-    arr[pos2] = temp
-
-
-def reverse_list(arr):
+def binary_search(arr, target):
     start = 0
-    end = len(arr)-1
-    while start < end:
-        swap(arr, start, end)
-        start += 1
-        end -= 1
+    end = len(arr) - 1
+
+    is_asc = arr[start] < arr[end]
+
+    while start <= end:
+        mid = int(start + (end - start) / 2)
+        if target == arr[mid]:
+            return mid
+        if is_asc:
+            if target > arr[mid]:
+                start = mid + 1
+            else:
+                end = mid - 1
+        else:
+            if target > arr[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+    return -1
 
 
-reverse_list(nums)
-print(nums)
+print(binary_search(nums, 1))

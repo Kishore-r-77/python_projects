4041c3cb01c04610af337ec1aae3339ff44bfa3f
Kishore-r-77 <kishorework77@gmail.com>
2023-12-18 16:25:14 +0530

    tested basic recursion in python

--- practice/main.py
+++ practice/main.py
@@ -1,12 +1,9 @@
-nums = [1, 2, 5, 9, 22]
+def display(n):
+    if n == 5:
+        return
+
+    print("Hello Buddy")
+    display(n+1)
 
 
-def two_sum(arr, target):
-    for i in range(len(arr)):
-        for j in range(len(arr)):
-            if target == arr[i] + arr[j]:
-                return [i, j]
-    return [-1, -1]
-
-
-print(two_sum(nums, 10))
+display(1)
